698e97a49655a66ab6d65c57e0d216c94d85a0b4
izzlan-bigpayme <izzlan.mohdanuar@bigpayme.com>
2025-09-20 17:48:43 +0800

    Enhancement

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://beginnerIzz.github.io/portfolio", // ✅ Required for GitHub Pages
+  outDir: "dist",
   vite: {
     plugins: [tailwindcss()],
   },
